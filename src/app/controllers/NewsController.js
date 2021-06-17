158aec807b9128a3ee4374e4cd9e2679da57457c
nguyenxuanda0550 <85298850+nguyenxuanda0550@users.noreply.github.com>
2021-06-18 01:23:16 +0700

    Create routes , app/Controleer

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,13 @@
+class NewsController {
+
+    //[GET] /News
+    index(req, res) {
+        res.render('news');
+    }
+
+    show(req, res) {
+        res.send('New Detail');
+    }
+}
+
+module.exports = new NewsController;
